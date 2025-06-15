49dbe5bbb9c83155d45f78fd1a3aa2e84019147c
Talha2195 <143935671+Talha2195@users.noreply.github.com>
2025-06-15 13:35:26 +0300

    updated redirect uponn failed login

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -90,7 +90,7 @@ export default function Profile() {
           setLikedPosts(likedPosts)
           setLikedComments(likedComments)
         } else {
-          router.push("/loginPage")
+          router.push("/index")
         }
       } catch (error) {
         console.error("Error loading profile:", error)
